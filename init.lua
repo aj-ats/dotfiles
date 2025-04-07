02403f857654c7d42b8f8eb5d88e535fecc90d60
aj-ats <andowens@advancedtech.com>
2025-04-07 13:35:47 -0500

    init commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Begin init.lua 
+--
+
+-- require("lua.config.lazy").setup("plugins")
+-- mouse mode off
+vim.opt.mouse = ""
+-- tokyo color sheme 
+    
+-- tabs n shit
+vim.opt.shiftwidth = 2  
+-- lazy.nvim 
+-- End Init.lua
